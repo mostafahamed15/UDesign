0c519e944353da73e7a6d3015bbc6c7da940f606
mostafahamed <mostafahamed73@yahoo.com>
2018-04-11 14:39:12 -0700

    brands finish design

--- src/app/components/brands/brands.component.ts
+++ src/app/components/brands/brands.component.ts
@@ -3,9 +3,6 @@ import { BrandService } from '../../services/brand.service'
 import { SlickModule } from 'ngx-slick';
 
 
-
-
-
 @Component({
   selector: 'app-brands',
   templateUrl: './brands.component.html',
@@ -17,7 +14,7 @@ export class BrandsComponent {
   public title:string=null
   private last:number = 0;
   private hasMore:boolean = true;
-    slideConfig = {'slidesToShow': 6, 'slidesToScroll': 1};
+    slideConfig = {'slidesToShow': 4, 'slidesToScroll': 1};
 
 
   constructor( public  brandService: BrandService) {
