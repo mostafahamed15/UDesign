a23fa91617a9ff96759849bfa804d96fa2b24a3e
mostafahamed <mostafahamed73@yahoo.com>
2018-04-12 10:28:51 -0700

    new featured toprated finished

--- src/app/components/new-featured-toprated/new-featured-toprated.component.ts
+++ src/app/components/new-featured-toprated/new-featured-toprated.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { CategoryService } from '../../services/category.service';
+import { PackageService } from '../../services/package.service';
+
+import {NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-new-featured-toprated',
@@ -7,11 +11,29 @@ import { ProductService } from '../../services/product.service';
   styleUrls: ['./new-featured-toprated.component.css']
 })
 export class NewFeaturedTopratedComponent implements OnInit {
-  public products:ProductsResponse;
-  constructor(private productService:ProductService) {
+  public products:any[]=[];
+  public category: object = {
+    categories:[]
+  };
+  public package: any;
+  
+  constructor(private productService:ProductService,
+    config: NgbTabsetConfig,
+    public categoryService:CategoryService,
+  public packageService:PackageService ) {
+    config.justify = 'center';
+      config.type = 'pills';
     this.productService.topProudcts().subscribe(res => {
       this.products = res['data']
       console.log(this.products)
+    },err => console.log(err));
+    this.categoryService.get('data').subscribe(res => {
+      this.category = res['data'];
+      // console.log(this.category);
+    },err => console.log(err));
+    this.packageService.topPackages().subscribe(res => {
+      this.package = res['data'];
+      console.log('this is what i want',this.package);
     },err => console.log(err));
    }
 
