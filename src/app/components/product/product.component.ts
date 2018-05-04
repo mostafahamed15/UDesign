ce9566dedfb4ff316f5b7c7609b154aa9daa1b0d
mostafahamed <mostafahamed73@yahoo.com>
2018-05-03 17:50:50 -0700

    product page changed

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -10,45 +10,6 @@ import {CartService} from '../../services/cart.service';
 })
 export class ProductComponent {
 
-// id: any;
-// details: any;
-// like: any;
-// public isLiked=true;
-//   constructor( private route: ActivatedRoute,
-//   public product: ProductService) { 
-//     this.route.params.subscribe(params=>{
-//       if(params['id']){
-//           this.id = params['id'];
-//           this.getDetails();
-//       }
-//   });
-// }
-//   getDetails(){
-// console.log(this.id)
-//     this.product.show(this.id).subscribe((res)=>{
-
-//         this.details = res['data']['product'];
-//         console.log(this.details);
-//     },(err)=>{
-
-//         console.log(err)
-//     }
-// )};
-// addLike(){
-
-    
-//     if (this.isLiked){
-//         this.details.likes += 1;
-//         this.isLiked = !this.isLiked;
-//     }else{
-//         this.details.likes -= 1;
-//         this.isLiked = !this.isLiked;
-//     }
-// console.log( this.details.likes);
-
-// }
-
-//}
 public nid:number;
 private id:any;
 private response:ProductResponse;
